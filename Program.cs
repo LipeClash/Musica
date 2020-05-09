020430f7c6be760533abed0266fb953f7c0496ed
LipeClash <felipeclaudino2003@hotmail.com>
2020-05-08 23:38:51 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace auladepc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("O nome dela e Jennifer!");
+        }
+    }
+}
